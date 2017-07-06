86f130c120b095a5027d9d92bb06a6a2e5f4751b
Dima <Dima@Home>
2017-07-06 17:45:50 +0300

    Cleaning

--- WebNew/src/by/htp/library/controller/CanNotCreateSource.java
+++ WebNew/src/by/htp/library/controller/CanNotCreateSource.java
@@ -4,8 +4,17 @@ public class CanNotCreateSource extends RuntimeException {
 
 	private static final long serialVersionUID = 9099693622276124572L;
 	
-	public CanNotCreateSource(Exception e) {
-			super(e);
+	public CanNotCreateSource() {
+			super();
 			}
+	public CanNotCreateSource(String message) {
+		super(message);
+		}
+	public CanNotCreateSource(Exception e) {
+		super(e);
+		}
+	public CanNotCreateSource(String message,Exception e) {
+		super(message,e);
+		}
 	
 }
